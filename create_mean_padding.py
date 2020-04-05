693657054fe4a72809cfdab4ed757d58b1275c0c
bastiandavid <bastian.david@ukbonn.de>
2020-04-05 18:45:29 +0200

    Creating axial paddings and fixing small os.makedirs()-error

--- create_mean_padding.py
+++ create_mean_padding.py
@@ -14,14 +14,21 @@ import numpy as np
 #import matplotlib.pyplot as plt
 from PIL import Image
 
-OUTPATH='/home/bdavid/Deep_Learning/playground/fake_flair_2d/png_cor/'
-INPATH='/home/bdavid/Deep_Learning/playground/fake_flair_2d/png_cor/'
-modalities = ['FLAIR',
-'T1']
+INPUT_CHANNELS = 7
+if INPUT_CHANNELS % 2 == 0:
+    print('Even no. of slices not supported, setting INPUT_CHANNELS to ',INPUT_CHANNELS+1)
+    INPUT_CHANNELS += 1
 
-folders=['test','train','val']
+OUTPATH='/home/bdavid/Deep_Learning/playground/fake_flair_2d/png_axial/'
+INPATH='/home/bdavid/Deep_Learning/playground/fake_flair_2d/png_axial/'
+modalities = ['FLAIR','T1']
+
+folders=['test','train']
 subjid=re.compile('([0-9]+)_')
 for modality in modalities:
+    
+    os.makedirs(os.path.join(OUTPATH,modality+'_paddings'))
+    
     for folder in folders:
         
         path_to_images=os.path.join(INPATH,modality,folder)
@@ -31,8 +38,8 @@ for modality in modalities:
         
         
         for subject in ids:
-            first_slices=sorted(glob.glob(os.path.join(path_to_images,subject)+'*'))[0:7]
-            last_slices=sorted(glob.glob(os.path.join(path_to_images,subject)+'*'))[-7:]
+            first_slices=sorted(glob.glob(os.path.join(path_to_images,subject)+'*'))[0:INPUT_CHANNELS]
+            last_slices=sorted(glob.glob(os.path.join(path_to_images,subject)+'*'))[-INPUT_CHANNELS:]
             
             first_mean=np.average(np.array([imageio.imread(im) for im in first_slices]),axis=0)
             last_mean=np.average(np.array([imageio.imread(im) for im in last_slices]),axis=0)
