971961a0be8e7041174aee5b617eb904d8412537
bastiandavid <bastian.david@ukbonn.de>
2020-05-26 18:19:38 +0200

    Added weight calculation based on mri_robust_register and parallelized processing

--- postprocessing/preprocessing_for_deepmedic.sh
+++ postprocessing/preprocessing_for_deepmedic.sh
@@ -11,6 +11,8 @@ BASE_DIR=/home/bdavid/Deep_Learning/data/bonn/FCD/iso_FLAIR/nii
 REAL_T1_DIR=${BASE_DIR}/T1
 REAL_FLAIR_DIR=${BASE_DIR}/FLAIR
 GAN_INPUT_T1_DIR=${BASE_DIR}/gan_input_T1
+GAN_INPUT_FLAIR_DIR=${BASE_DIR}/gan_input_FLAIR
+SYNTH_FLAIR_DIR=${BASE_DIR}/synth_FLAIR
 DIFF_DIR=${BASE_DIR}/diff_real_FLAIR-synth_FLAIR
 MAP_DIR=${BASE_DIR}/MAP_DeepFCD
 
@@ -21,15 +23,10 @@ DEEPMEDIC_INPUT=${BASE_DIR}/deepmedic_input
 tmp_dir=${BASE_DIR}/tmp
 mkdir -p $MATRICES_DIR $MASK_DIR $DEEPMEDIC_INPUT $tmp_dir
 
-# define subjects
+# define subjects (not necessary if using parallelized wrapper script)
 #SUBJECTS=$(ls ${T1_DIR}| cut -d'_' -f1)
-SUBJECTS=11375
+SUBJECTS=$1
 
-echo ""
-echo "#########################################################################"
-echo "#################       PRE-DEEPMEDIC PROCESSING       ##################"
-echo "#########################################################################"
-echo ""
 for sbj in $SUBJECTS
 do
   echo "Processing $sbj"
@@ -39,7 +36,8 @@ do
 
   flirt -in ${DIFF_DIR}/${sbj}* -ref ${REAL_T1_DIR}/${sbj}_T1.nii.gz -applyxfm -init ${MATRICES_DIR}/${sbj}_gan_input_T1_2_T1.mat -nosearch -noresampblur -cost normmi -interp spline -out ${DEEPMEDIC_INPUT}/${sbj}_diff
   echo "fast"
-  fast -g -o ${tmp_dir}/${sbj} ${REAL_T1_DIR}/${sbj}_T1.nii.gz
+  bet ${REAL_T1_DIR}/${sbj}_T1.nii.gz ${tmp_dir}/${sbj}_bet_T1 -R
+  fast -g -o ${tmp_dir}/${sbj} ${tmp_dir}/${sbj}_bet_T1
 
   fslmaths ${tmp_dir}/${sbj}_seg_1 -add ${tmp_dir}/${sbj}_seg_2 ${tmp_dir}/${sbj}_gmwm
   fslmaths ${tmp_dir}/${sbj}_gmwm -fillh ${tmp_dir}/${sbj}_gmwm
@@ -61,18 +59,37 @@ do
   fslmaths ${REAL_FLAIR_DIR}/${sbj}* -sub $mean -div $std ${DEEPMEDIC_INPUT}/${sbj}_FLAIR
 
   # normalizing junction map
-  read -r mean std <<< $(fslstats ${MAP_DIR}/T1_${sbj}_junction_z_score -k ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded -m -s)
+  imcp ${MAP_DIR}/T1_${sbj}_junction_z_score ${tmp_dir}/
+  fslcpgeom ${REAL_T1_DIR}/${sbj}_T1.nii.gz ${tmp_dir}/T1_${sbj}_junction_z_score
 
-  fslmaths ${MAP_DIR}/T1_${sbj}_junction_z_score -sub $mean -div $std -mul ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded ${DEEPMEDIC_INPUT}/${sbj}_junction
+  read -r mean std <<< $(fslstats ${tmp_dir}/T1_${sbj}_junction_z_score -k ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded -m -s)
+
+  fslmaths ${tmp_dir}/T1_${sbj}_junction_z_score -sub $mean -div $std -mul ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded ${DEEPMEDIC_INPUT}/${sbj}_junction
 
   # normalizing extension map
-  read -r mean std <<< $(fslstats ${MAP_DIR}/T1_${sbj}_extension_z_score -k ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded -m -s)
+  imcp ${MAP_DIR}/T1_${sbj}_extension_z_score ${tmp_dir}/
+  fslcpgeom ${REAL_T1_DIR}/${sbj}_T1.nii.gz ${tmp_dir}/T1_${sbj}_extension_z_score
 
-  fslmaths ${MAP_DIR}/T1_${sbj}_extension_z_score -sub $mean -div $std -mul ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded ${DEEPMEDIC_INPUT}/${sbj}_extension
+  read -r mean std <<< $(fslstats ${tmp_dir}/T1_${sbj}_extension_z_score -k ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded -m -s)
+
+  fslmaths ${tmp_dir}/T1_${sbj}_extension_z_score -sub $mean -div $std -mul ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded ${DEEPMEDIC_INPUT}/${sbj}_extension
 
   # normalzing thickness map
-  read -r mean std <<< $(fslstats ${MAP_DIR}/T1_${sbj}_thickness_z_score -k ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded -m -s)
+  imcp ${MAP_DIR}/T1_${sbj}_thickness_z_score ${tmp_dir}/
+  fslcpgeom ${REAL_T1_DIR}/${sbj}_T1.nii.gz ${tmp_dir}/T1_${sbj}_thickness_z_score
 
-  fslmaths ${MAP_DIR}/T1_${sbj}_thickness_z_score -sub $mean -div $std -mul ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded ${DEEPMEDIC_INPUT}/${sbj}_thickness
+  read -r mean std <<< $(fslstats ${tmp_dir}/T1_${sbj}_thickness_z_score -k ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded -m -s)
+
+  fslmaths ${tmp_dir}/T1_${sbj}_thickness_z_score -sub $mean -div $std -mul ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded ${DEEPMEDIC_INPUT}/${sbj}_thickness
+
+  # creating weight map using mri_robust_register
+  mri_robust_register --mov ${GAN_INPUT_FLAIR_DIR}/${sbj}* --dst ${SYNTH_FLAIR_DIR}/${sbj}* --lta ${tmp_dir}/${sbj}.lta --weights  ${tmp_dir}/${sbj}_weights.nii --satit
+
+  flirt -in ${tmp_dir}/${sbj}_weights.nii -ref ${REAL_T1_DIR}/${sbj}_T1.nii.gz -applyxfm -init ${MATRICES_DIR}/${sbj}_gan_input_T1_2_T1.mat -nosearch -noresampblur -cost normmi -interp spline -out ${tmp_dir}/${sbj}_weights_reg
+
+  read -r mean std <<< $(fslstats ${tmp_dir}/${sbj}_weights_reg -k ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded -m -s)
+
+  fslmaths ${tmp_dir}/${sbj}_weights_reg -sub $mean -div $std -mul ${DEEPMEDIC_INPUT}/${sbj}_gmwm_eroded ${DEEPMEDIC_INPUT}/${sbj}_weights
+
 
 done
---
+++ postprocessing/wrapper_preproc_deepmedic.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Wrapper for preprocessing_for_deepmedic.sh
+
+# Written by: Bastian David, M.Sc.
+echo ""
+echo "#########################################################################"
+echo "#################       PRE-DEEPMEDIC PROCESSING       ##################"
+echo "#########################################################################"
+echo ""
+
+# define subjects here
+SUBJECTS=555-nase
+
+max_cores=$(grep -c ^processor /proc/cpuinfo)
+echo "How many cores shall be used? [1-$max_cores]:"
+read cores
+
+
+
+if [[ "$cores" =~ ^[0-9]+$ ]] && [ "$cores" -ge 1 -a "$cores" -le $max_cores ];
+then
+    echo ""
+    echo "$cores cores will be used."
+
+else
+    echo ""
+    echo "Invalid input - Terminating script."
+    exit 1
+fi
+
+
+echo ""
+echo "STARTING PARALLEL PROCESSING"
+echo ""
+echo "Processing list:"
+echo ""
+
+
+xargs -a $SUBJECTS -n 1 -P $cores ./preprocessing_for_deepmedic.sh #| grep "Processing"
